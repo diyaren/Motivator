ab03ba16574e5a1a9d847ef83a3003d83d915387
Navjashan Singh <navjashansingh@gmail.com>
2017-03-16 18:00:55 -0700

    ToDo done

---
+++ app/src/main/java/com/example/motivator/TaskContract.java
@@ -0,0 +1,19 @@
+package com.example.motivator;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Navjashan on 16/03/2017.
+ */
+
+public class TaskContract {
+
+    public static final String DB_NAME = "com.example.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+        public static final String COL_TASK_TITLE = "title";
+    }
+
+}
